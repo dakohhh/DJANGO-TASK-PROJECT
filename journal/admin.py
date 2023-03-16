394aeb65a41b38299f476bc3fae47153c5ef0b18
Dakoh Wisdom <wiizzydreadmill@gmail.com>
2023-03-15 16:09:18 -1200

    first commit

---
+++ journal/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+
+
+from .models import Task, Review
+
+admin.site.register(Task)
+admin.site.register(Review)
---
+++ journal/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+
+    path('', views.home, name="home"),
+
+    path("task", views.task, name="task"),
+
+    path("create_task", views.create_task, name="create_task"),
+
+    path("update_task/<int:id>", views.update_task, name="update_task"), 
+
+    path("delete_task/<int:id>", views.delete_task, name="delete_task"),
+
+    path("register", views.register, name="register")
+
+
+
+]
