d26e25542e6c1fe5d2aec65518f14c6b18ec9dfe
Dakoh Wisdom <wiizzydreadmill@gmail.com>
2023-03-25 08:08:19 -1200

    added s3 static and file configuration

--- ThoughtProject/settings.py
+++ ThoughtProject/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
 
+    'storages',
+
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACK = "boostrap5"
@@ -173,3 +175,18 @@ DEFAULT_FROM_EMAIL =  os.getenv("MAIL_USERNAME")
 
 # AWS SETTINGS
 AWS_S3_FILE_OVERWRITE = False
+
+# AWS S3 CONFIGURATION
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
