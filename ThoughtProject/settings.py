fa08f54c0e8873f85822c72b5ffea71072366018
Dakoh Wisdom <wiizzydreadmill@gmail.com>
2023-03-25 15:17:22 -1200

    added statics and templates

--- ThoughtProject/settings.py
+++ ThoughtProject/settings.py
@@ -27,10 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.43.93", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
+
+# CSRF_TRUSTED_ORIGINS = ['https://']
 
 # Application definition
 
@@ -120,13 +122,6 @@ DATABASES = {
         'PORT': os.getenv("DB_PORT")
     }
 }
-
-
-
-
-
-
-
 
 
 
